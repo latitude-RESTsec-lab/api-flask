ace27802f43d306305eebf7b0e57ddaa7fa5b94f
Lucas Martins <lucas.martins@redes.unb.br>
2018-01-16 14:53:46 -0200

    parametrizing the debug mode

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ app.register_blueprint(con.pessoal_controllers)
 
 if __name__ == '__main__':
     # configuring the parameters parser and storing parameters in global vars
-    parser = argparse.ArgumentParser(description='API Servidor to provide employee\'s data.')
+    parser = argparse.ArgumentParser(description='"API Servidor" to provide employee\'s data.')
 
     parser.add_argument("-s", "--servername", metavar='server_name', 
                         help='Name of the database_server')
@@ -25,9 +25,10 @@ if __name__ == '__main__':
                         help="Username to access the database", metavar="username")
     parser.add_argument("-p", "--password", 
                         help="User's password to acess the database", metavar="user_password")
+    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
     con.configure_params(args.servername, args.database, args.username, args.password)
 
     # starting the web server
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=args.debug, host='0.0.0.0', port=8000)
