93d1bbe516de014330e80f9c1e933fa561c16fb9
Lucas Martins <lucas.martins@redes.unb.br>
2018-01-16 16:40:37 -0200

    including some validation (WIP)

--- src/controllers/pessoal.py
+++ src/controllers/pessoal.py
@@ -56,8 +56,41 @@ def get_employee_by_id_api(mat_servidor=None):
         else:
             return "Bad Request", 400
 
-# web service API servidores
+# web service - create new employee
 @pessoal_controllers.route('/api/servidor', methods=['POST'])
 def create_a_new_employee_api():
-    print "post de servidor"
+    '''API to create a new employee record using the provided data.
+
+    Requires JSON formatted request containing the employ data listed 
+    below in the sample_json var.
+	
+    Example: 
+	curl -H "Content-Type: application/json" -X POST -d '{"id_servidor": 4321, "siape": 123456, "id_pessoa": 1234, "matricula_interna": 54321, "nome": "João da Silva", "data_nascimento": "1970-01-31", "sexo": "M"}' http://localhost:8000/api/servidor/
+    
+    sample_json = {
+	"id_servidor": 4321,
+	"siape": 123456,
+	"id_pessoa": 1234,
+	"matricula_interna": 54321,
+	"nome": "João da Silva",
+	"data_nascimento": "1970-01-31",
+	"sexo": "M"
+}
+	'''
+
+    # TODO test if the data is JSON
+    new_employee_data = request.json
+    print "post de servidor={}".format(new_employee_data)
+    required_data = ['nome', 'siape', 'data_nascimento', 'sexo', 'id_servidor', 'id_pessoa']
+
+    diferenca = set(required_data) - set(new_employee_data)
+    if diferenca:
+        message = "faltou dado = {}".format(diferenca)
+        return "Bad request\n{}".format(message), 400
+    
+
+
     return "Ok", 200
+    # TODO test if the response can be JSON
+    #if not validate_suported_mime_type():
+    #    return "Unsupported Media Type", 415
