63d770fd988446910ad457431a3850a9b9d92a0e
Lucas Martins <lucas.martins@redes.unb.br>
2018-01-24 07:55:23 -0200

    removing hard-coded python interpreter reference

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 18 12:15:29 2017
--- src/db/db.py
+++ src/db/db.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 19 09:46:12 2018
