aee598f3d03ae2df1b6bfc7c9e04e949e0998503
Lucas Martins <lucas.martins@redes.unb.br>
2018-01-24 07:47:11 -0200

    including API port parametrization

--- src/main.py
+++ src/main.py
@@ -18,17 +18,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='"API Servidor" to provide employee\'s data.')
 
     parser.add_argument("-s", "--servername", metavar='server_name', 
-                        help='Name of the database_server')
+                        help='Name of the database host server')
     parser.add_argument("-d", "--database", 
                         help="Name of the database", metavar="database_name")
     parser.add_argument("-u", "--username", 
                         help="Username to access the database", metavar="username")
-    parser.add_argument("-p", "--password", 
+    parser.add_argument("-w", "--password", 
                         help="User's password to acess the database", metavar="user_password")
     parser.add_argument('--debug', action='store_true')
+    parser.add_argument("-p", "--port", type=int, default=8000,
+                        help="Port the program will try to use to serve de API", metavar="api_port")
     args = parser.parse_args()
 
     con.configure_params(args.servername, args.database, args.username, args.password)
 
     # starting the web server
-    app.run(debug=args.debug, host='0.0.0.0', port=8000)
+    app.run(debug=args.debug, host='0.0.0.0', port=args.port)
